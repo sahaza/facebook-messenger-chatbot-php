33331c4b780bc71fcaf5cb3d1ea2014849691c77
Sahaza Marline <contact@sahaza.mg>
2017-03-05 07:48:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/** 
+ * Webhook file. User can access via /messenger/
+ */
+
+if ( isset( $_REQUEST['hub_verify_token'] ) && $_REQUEST['hub_verify_token'] == 'FacebookMessengerBots' ) 
+{
+	echo $_REQUEST['hub_challenge'];
+
+	exit;
+}
+
+require_once 'loader.php';
+
+require_once 'examples/example.php';
